7812b6948d70b94c03efaa5da299e3ee3cb98836
Qsong <vipqinsong@gmail.com>
2020-01-15 18:06:49 +0800

    tcp

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,14 +1,4 @@
-
-//------------------------BILIBLI-------------------------------
-
-//硬件连接说明：
-//MAX7219 --- ESP8266
-//  VCC   --- 3V(3.3V)
-//  GND   --- G (GND)
-//  DIN   --- D7(GPIO13)
-//  CS    --- D1(GPIO5)
-//  CLK   --- D5(GPIO14)
-
+//-------------------------基础框架------------------------------
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
@@ -17,13 +7,15 @@
 //WIFI信息
 String ssid = "ICBM";          //WiFi名
 String password = "Androids";  //WiFi密码
-String biliuid = "423895";         //bilibili UID
 
-
-const unsigned long HTTP_TIMEOUT = 5000;
-HTTPClient http;
-String response;
-int follower = 0;
+//-------------------------DigitalTube------------------------------
+//硬件连接说明：
+//MAX7219 --- ESP8266
+//  VCC   --- 3V(3.3V)
+//  GND   --- G (GND)
+//  DIN   --- D7(GPIO13)
+//  CS    --- D1(GPIO5)
+//  CLK   --- D5(GPIO14)
 const int pinTube = 5;
 const int scanLimit = 7;
 
@@ -35,10 +27,11 @@ const int scanLimit = 7;
 
 #include <SimpleDHT.h>
 
-const int pinSwitch = D0;
 const int pinDHT11 = D4;
 SimpleDHT11 dht11(pinDHT11);
-
+// read without samples.
+byte temperature = 0;
+byte humidity = 0;
 
 //-------------------------网络时间------------------------------
 
@@ -56,6 +49,10 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntpServer, gmtOffset_sec, 60000);
 
 
+//-------------------------开关------------------------------
+const int pinSwitch = D0;
+byte isSwitchOpen = 0;
+
 //-------------------------logic------------------------------
 
 void setup()
@@ -65,7 +62,7 @@ void setup()
   while (!Serial)
     continue;
 
-  Serial.println("\nbilibili fans monitor, version v1.2");
+  Serial.println("\nQsong project for esp8266, version v1.0");
 
   //init digitalTube
   SPI.begin();
@@ -78,13 +75,13 @@ void setup()
   sendTubeCommand(9, 255);        //DecodeMode,Code B decode for digits 7-0
   digitalWrite(pinTube, HIGH);
   initDisplay(0);
-  Serial.println("LED Ready");
+  Serial.println("DigitalTube Ready");
 
-  //init DHT11
+  //init switch
   pinMode(pinSwitch, OUTPUT);
-  digitalWrite(pinSwitch, HIGH);
-  getDHT11();
+  switchPin(pinSwitch, isSwitchOpen);
 
+  getDHT11();
   connWifi();
 }
 
@@ -95,27 +92,19 @@ void loop() {
   //  return;
 
   if (WiFi.status() == WL_CONNECTED) {
-
     for (int i = 0; i < 4; i++) {
       getTime();
       delayAndHandleTask(1000);
     }
-
-    if (getJson()) {
-      if (parseJson(response)) {
-        displayNumber(follower);
-      }
-    }
-  } else {
-    Serial.println("[WiFi] Waiting to reconnect...");
-    errorCode(0x1);
-  }
-
-  delayAndHandleTask(1000);
-
-  for (int i = 0; i < 1; i++) {
     getDHT11();
     delayAndHandleTask(1000);
+    runBili();
+    delayAndHandleTask(1000);
+  } else {
+    Serial.println("[WiFi] Waiting to reconnect...");
+    //    errorCode(0x1);
+    initDisplay(millis() / 100);
+    delay(100);
   }
 
 }
@@ -147,11 +136,7 @@ bool getDHT11() {
   // start working...
   Serial.println("Sample DHT11...");
 
-  // read without samples.
-  byte temperature = 0;
-  byte humidity = 0;
   int err = SimpleDHTErrSuccess;
-
   if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err="); Serial.println(err);
     displayDHT11(temperature, humidity);
@@ -160,8 +145,7 @@ bool getDHT11() {
 
   Serial.printf("DHT11 Read OK: %d *C, %d H\n", (int) temperature, (int) humidity);
   displayDHT11(temperature, humidity);
-  digitalWrite(pinSwitch, humidity > 60 ? LOW : HIGH);
-
+  uploadData();
   return true;
 }
 
@@ -186,6 +170,10 @@ bool getTime() {
   return true;
 }
 
+
+void switchPin(int pinSwitch, byte low) {
+  digitalWrite(pinSwitch, low ? LOW : HIGH);
+}
 //空闲时间检测是否有消息任务
 bool delayAndHandleTask(int timeout) {
   Serial.print("delayAndHandleTask...");
@@ -208,7 +196,6 @@ void checkSerialIO() {
     char c = Serial.read();
     Serial.print("I received:"); //print I received
     Serial.println(c); //send what you read
-    if (c == 't')testDisplay();
     if (c == 's')smartConfig(30);
   }
 }
@@ -217,19 +204,20 @@ void checkSerialIO() {
 WiFiClient client;
 //设备唯一标识,后台提前入库
 const char* DEVICE_ID = "S5FE62HHYDBI";
-String host = "192.168.168.119";
-const uint16_t port = 9000;
+String host = "api.reol.top";
+const uint16_t port = 8899;
 //检测TCP连接消息
 bool checkTCPIO() {
   //检测建立连接
   if (!client.connected()) {
     if (!client.connect(host, port)) {//5s timeout
-      Serial.println("TCP connection failed->" + host + ":" + port);
-      String id = DEVICE_ID;
-      sendTCP("{\"id\":\"" + id + "\"}");
+      Serial.println("TCP Connection Failed->" + host + ":" + port);
       return false;
     } else {
-      Serial.println("TCP connection OK->" + host + ":" + port);
+      //建立连接发送设备id,授权
+      String id = DEVICE_ID;
+      sendTCP("{\"type\":0,\"id\":\"" + id + "\"}");
+      Serial.println("TCP Connection OK->" + host + ":" + port);
     }
   } else {
     //tcp连接建立
@@ -242,79 +230,45 @@ void listenTCP() {
   //有消息
   if (client.available() > 0) {
     //默认超时1秒
-    String line = client.readStringUntil('\n');
-    Serial.println("TCP read->" + line);
-    client.print(line);
+    String json = client.readStringUntil('\n');
+    Serial.println("TCP Read->" + json);
+
+    const size_t capacity = JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 70;
+    DynamicJsonDocument doc(capacity);
+    deserializeJson(doc, json);
+    int type = doc["type"];
+    if (type == -1) {//控制开关
+      isSwitchOpen = doc["data"];
+      switchPin(pinSwitch, isSwitchOpen);
+    }
+    uploadData();
   }
 }
+//上报设备所有信息,后期复杂可根据命令上报对应消息
+void uploadData() {
+  StaticJsonDocument<200> doc;
+  doc["type"] = 1;
+  doc["switch"] = isSwitchOpen;
+  doc["temp"] = temperature;
+  doc["humi"] = humidity;
+  String output;
+  serializeJson(doc, output);
+  sendTCP(output);
+}
+
 //发送TCP消息
 bool sendTCP(String msg) {
-  Serial.println("sending data to server->" + msg);
   if (client.connected()) {
+    Serial.println("TCP Send OK->" + msg);
     client.println(msg);
     return true;
   } else {
+    Serial.println("TCP Send Fail->" + msg);
     return false;
   }
 }
 //-------------------------TCP END------------------------------
 
-bool getJson()
-{
-  String api = "http://api.bilibili.com/x/relation/stat?vmid=" + biliuid;
-  Serial.println("Request:" + api);
-  bool r = false;
-  http.setTimeout(HTTP_TIMEOUT);
-  http.begin(api);
-  int httpCode = http.GET();
-
-  if (httpCode == HTTP_CODE_OK) {
-    response = http.getString();
-    Serial.println(response);
-    r = true;
-  } else {
-    Serial.printf("[HTTP] GET JSON failed, error: %s\n", http.errorToString(httpCode).c_str());
-    errorCode(0x2);
-    r = false;
-  }
-  http.end();
-  return r;
-}
-
-bool parseJson(String json)
-{
-  const size_t capacity = JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 70;
-  DynamicJsonDocument doc(capacity);
-  deserializeJson(doc, json);
-
-  int code = doc["code"];
-  const char *message = doc["message"];
-
-  if (code != 0) {
-    Serial.print("[API]Code:");
-    Serial.print(code);
-    Serial.print(" Message:");
-    Serial.println(message);
-    errorCode(0x3);
-    return false;
-  }
-
-  JsonObject data = doc["data"];
-  unsigned long data_mid = data["mid"];
-  int data_follower = data["follower"];
-  if (data_mid == 0) {
-    Serial.println("[JSON] FORMAT ERROR");
-    errorCode(0x4);
-    return false;
-  }
-  Serial.print("UID: ");
-  Serial.print(data_mid);
-  Serial.print(" follower: ");
-  Serial.println(data_follower);
-
-  follower = data_follower;
-  return true;
-}
 
 void displayNumber(int number) //display number in the middle
 {
@@ -367,24 +321,6 @@ void initDisplay(int pro)
     sendTubeCommand(3, pro > 1 & pro < 5 ? 0xa : 0xf);
     sendTubeCommand(2, pro > 2 & pro < 6 ? 0xa : 0xf);
     sendTubeCommand(1, pro > 3 & pro < 7 ? 0xa : 0xf);
-  }
-}
-
-void testDisplay()
-{
-  for (int i = 0; i < 256; i++) {
-    sendTubeCommand(8, i / 1000 % 10);
-    sendTubeCommand(7, i / 100 % 10);
-    sendTubeCommand(6, i / 10 % 10);
-    sendTubeCommand(5, i % 10);
-    sendTubeCommand(4, 0xf);
-    sendTubeCommand(3, 0xf);
-    sendTubeCommand(2, 0xf);
-    sendTubeCommand(1, i);
-    delay(500);
-    if (Serial.available()) {
-      if (Serial.read() == 't') return;
-    }
   }
 }
 
@@ -443,4 +379,78 @@ void smartConfig(int timeout)
     Serial.println("\nSmartConfig Fail->Timeout!");
   }
 
+}
+
+
+//------------------------BILIBLI-------------------------------
+
+String biliuid = "423895";         //bilibili UID
+const unsigned long HTTP_TIMEOUT = 5000;
+HTTPClient http;
+String response;
+int follower = 0;
+
+void runBili() {
+  if (getJson()) {
+    if (parseJson(response)) {
+      displayNumber(follower);
+    }
+  }
+}
+
+bool getJson()
+{
+  String api = "http://api.bilibili.com/x/relation/stat?vmid=" + biliuid;
+  Serial.println("Request:" + api);
+  bool r = false;
+  http.setTimeout(HTTP_TIMEOUT);
+  http.begin(api);
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK) {
+    response = http.getString();
+    Serial.println(response);
+    r = true;
+  } else {
+    Serial.printf("[HTTP] GET JSON failed, error: %s\n", http.errorToString(httpCode).c_str());
+    errorCode(0x2);
+    r = false;
+  }
+  http.end();
+  return r;
+}
+
+bool parseJson(String json)
+{
+  const size_t capacity = JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + 70;
+  DynamicJsonDocument doc(capacity);
+  deserializeJson(doc, json);
+
+  int code = doc["code"];
+  const char *message = doc["message"];
+
+  if (code != 0) {
+    Serial.print("[API]Code:");
+    Serial.print(code);
+    Serial.print(" Message:");
+    Serial.println(message);
+    errorCode(0x3);
+    return false;
+  }
+
+  JsonObject data = doc["data"];
+  unsigned long data_mid = data["mid"];
+  int data_follower = data["follower"];
+  if (data_mid == 0) {
+    Serial.println("[JSON] FORMAT ERROR");
+    errorCode(0x4);
+    return false;
+  }
+  Serial.print("UID: ");
+  Serial.print(data_mid);
+  Serial.print(" follower: ");
+  Serial.println(data_follower);
+
+  follower = data_follower;
+  return true;
 }
